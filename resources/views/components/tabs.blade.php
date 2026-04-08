dad4ba296b23fb19b1b13e9d66bba448b689f04c
Bogdan Matrosov <laroncool@gmail.com>
2026-04-08 20:14:01 +0400

    Поправил табы

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,12 +1,12 @@
 <div class="tabs-wrapper">
     <div class="tabs">
-        <a href="?tab=workers" class="tab {{ $activeTab == 'workers' ? 'active' : '' }}">
+        <a href="/" class="tab {{ request()->get('tab') == 'workers' || request()->get('tab') == null ? 'active' : '' }}">
             <i class="fas fa-users"></i> Редактор рабочих
         </a>
-        <a href="?tab=statistics" class="tab {{ $activeTab == 'statistics' ? 'active' : '' }}">
+        <a href="/?tab=statistics" class="tab {{ request()->get('tab') == 'statistics' ? 'active' : '' }}">
             <i class="fas fa-chart-bar"></i> Статистика группы
         </a>
-        <a href="/charts" class="tab">
+        <a href="/charts" class="tab {{ request()->routeIs('charts') ? 'active' : '' }}">
             <i class="fas fa-chart-line"></i> Графики
         </a>
     </div>
