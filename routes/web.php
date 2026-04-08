b9e6b5ba6aeebdd668ecac1d02f594acbf518563
Bogdan Matrosov <laroncool@gmail.com>
2026-04-08 20:13:05 +0400

    Переделал удаление рабочих и групп с GET на DELETE  и добавил csfr защиту

--- routes/web.php
+++ routes/web.php
@@ -6,44 +6,39 @@ use App\Http\Controllers\GroupController;
 use App\Http\Controllers\ProductivityController;
 use App\Http\Controllers\ImportExportController;
 use App\Http\Controllers\ChartController;
-use App\Http\Controllers\Api\CalculatorController;
 use Illuminate\Support\Facades\Route;
 
 // Главная страница
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Web
+// Группа маршрутов для рабочих
+Route::prefix('worker')->group(function () {
+    Route::post('/add', [WorkerController::class, 'store'])->name('worker.store');
+    Route::get('/edit/{id}', [WorkerController::class, 'edit'])->name('worker.edit');
+    Route::post('/update/{id}', [WorkerController::class, 'update'])->name('worker.update');
+    Route::delete('/delete/{id}', [WorkerController::class, 'destroy'])->name('worker.destroy');
+});
 
-// Рабочие
-Route::post('/worker/add', [WorkerController::class, 'addWorker']);
-Route::get('/worker/edit/{id}', [WorkerController::class, 'editWorker']);
-Route::post('/worker/update/{id}', [WorkerController::class, 'updateWorker']);
-Route::get('/worker/delete/{id}', [WorkerController::class, 'deleteWorker']);
-
-// Группы
-Route::post('/group/create', [GroupController::class, 'createGroup']);
-Route::get('/group/edit/{id}', [GroupController::class, 'editGroup']);
-Route::post('/group/update/{id}', [GroupController::class, 'updateGroup']);
-Route::get('/group/delete/{id}', [GroupController::class, 'deleteGroup']);
-
-// Состав группы
-Route::post('/group/{groupId}/add-worker', [GroupController::class, 'addToGroup']);
-Route::post('/group/{groupId}/remove-worker', [GroupController::class, 'removeFromGroup']);
-
-// Производительность
-Route::post('/group/{groupId}/productivity', [ProductivityController::class, 'saveProductivity']);
-Route::get('/group/{groupId}/calculate', [ProductivityController::class, 'calculate']);
-Route::get('/group/{groupId}/calculate-alternative', [ProductivityController::class, 'calculateAlternative']);
+// Группа маршрутов для групп
+Route::prefix('group')->group(function () {
+    Route::post('/create', [GroupController::class, 'store'])->name('group.store');
+    Route::get('/edit/{id}', [GroupController::class, 'edit'])->name('group.edit');
+    Route::post('/update/{id}', [GroupController::class, 'update'])->name('group.update');
+    Route::delete('/delete/{id}', [GroupController::class, 'destroy'])->name('group.destroy');
+    
+    // Состав группы
+    Route::post('/{groupId}/add-worker', [GroupController::class, 'addWorker'])->name('group.addWorker');
+    Route::post('/{groupId}/remove-worker', [GroupController::class, 'removeWorker'])->name('group.removeWorker');
+    
+    // Производительность
+    Route::post('/{groupId}/productivity', [ProductivityController::class, 'saveProductivity'])->name('productivity.save');
+    Route::get('/{groupId}/calculate', [ProductivityController::class, 'calculate'])->name('productivity.calculate');
+    Route::get('/{groupId}/calculate-alternative', [ProductivityController::class, 'calculateAlternative'])->name('productivity.calculateAlternative');
+});
 
 // Импорт/экспорт
-Route::get('/workers/export', [ImportExportController::class, 'exportWorkers']);
-Route::post('/workers/import', [ImportExportController::class, 'importWorkers']);
+Route::get('/workers/export', [ImportExportController::class, 'exportWorkers'])->name('workers.export');
+Route::post('/workers/import', [ImportExportController::class, 'importWorkers'])->name('workers.import');
 
 // Графики
-Route::get('/charts', [ChartController::class, 'charts']);
-
-// Api-маршруты
-Route::prefix('api')->group(function () {
-    Route::get('/calculate/{groupId}', [CalculatorController::class, 'calculate']);
-    Route::get('/calculate-alternative/{groupId}', [CalculatorController::class, 'calculateAlternative']);
-});
+Route::get('/charts', [ChartController::class, 'charts'])->name('charts');
