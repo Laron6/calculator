cb60cdcc8217b71c8aa115549fd38d03de36a942
Bogdan Matrosov <laroncool@gmail.com>
2026-04-14 00:34:19 +0400

    Добавил middleware для обновления времени последней активности пользователя

---
+++ app/Http/Middleware/TrackUserActivity.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Services\DeviceService;
+
+class TrackUserActivity
+{
+    protected $deviceService;
+    
+    public function __construct(DeviceService $deviceService)
+    {
+        $this->deviceService = $deviceService;
+    }
+    
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check()) {
+            $sessionId = $request->session()->getId();
+            $this->deviceService->updateActivity($sessionId);
+        }
+        
+        return $next($request);
+    }
+}
