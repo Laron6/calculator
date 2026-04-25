ef0fcdd4b40b6db99c303e86d648ce1011d24ee3
Bogdan Matrosov <laroncool@gmail.com>
2026-04-25 22:58:42 +0400

    другая формула

--- app/Services/ProductivityService.php
+++ app/Services/ProductivityService.php
@@ -8,35 +8,96 @@ use Illuminate\Support\Facades\Log;
 
 class ProductivityService
 {
-    public function saveProductivities($groupId, array $productivities)
+    public function saveProductivities($groupId, array $volumes, array $times)
     {
         $group = WorkGroup::find($groupId);
         if (!$group) {
+            Log::error('Группа не найдена', ['group_id' => $groupId]);
             return false;
         }
         
         $workerIds = $group->workers->pluck('id')->toArray();
+        $savedCount = 0;
         
-        foreach ($productivities as $workerId => $value) {
-            // Проверяем, что рабочий действительно входит в группу
-            if (!in_array($workerId, $workerIds)) {
+        foreach ($workerIds as $workerId) {
+            $volume = $volumes[$workerId] ?? null;
+            $time = $times[$workerId] ?? null;
+            
+            // Пропускаем если оба поля пустые
+            if (($volume === null || $volume === '') && ($time === null || $time === '')) {
                 continue;
             }
             
-            if ($value !== null && $value !== '') {
-                GroupProductivity::updateOrCreate(
-                    ['work_group_id' => $groupId, 'worker_id' => $workerId],
-                    ['value' => (float)$value]
-                );
+            // Преобразуем в числа
+            $volume = $volume !== null && $volume !== '' ? (float)$volume : 0;
+            $time = $time !== null && $time !== '' ? (float)$time : 0;
+            
+            // Рассчитываем производительность по формуле ПТ = V / T
+            $productivity = 0;
+            if ($time > 0 && $volume > 0) {
+                $productivity = $volume / $time;
             }
+            
+            GroupProductivity::updateOrCreate(
+                ['work_group_id' => $groupId, 'worker_id' => $workerId],
+                [
+                    'volume' => $volume,
+                    'time' => $time,
+                    'value' => round($productivity, 2)
+                ]
+            );
+            
+            $savedCount++;
         }
+        
+        Log::info('Данные производительности сохранены', [
+            'group_id' => $groupId,
+            'saved_count' => $savedCount
+        ]);
+        
         return true;
     }
     
+    /**
+     * Получение данных производительности для группы
+     * 
+     * @param int $groupId
+     * @return \Illuminate\Support\Collection
+     */
     public function getProductivities($groupId)
     {
         return GroupProductivity::where('work_group_id', $groupId)
             ->get()
             ->keyBy('worker_id');
+    }
+    
+    /**
+     * Получение только объёмов продукции для группы
+     */
+    public function getVolumes($groupId)
+    {
+        return GroupProductivity::where('work_group_id', $groupId)
+            ->pluck('volume', 'worker_id')
+            ->toArray();
+    }
+    
+    /**
+     * Получение только времени для группы
+     */
+    public function getTimes($groupId)
+    {
+        return GroupProductivity::where('work_group_id', $groupId)
+            ->pluck('time', 'worker_id')
+            ->toArray();
+    }
+    
+    /**
+     * Получение производительности для группы
+     */
+    public function getProductivityValues($groupId)
+    {
+        return GroupProductivity::where('work_group_id', $groupId)
+            ->pluck('value', 'worker_id')
+            ->toArray();
     }
 }
