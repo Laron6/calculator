caec708d6f592b420ace1a5dd76326162c7edeff
Bogdan Matrosov <laroncool@gmail.com>
2026-04-25 22:53:54 +0400

    передача productivities вместо bvec и decisions

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -23,16 +23,14 @@ class ChartController extends Controller
             $groups = $this->groupService->getAll();
             $selectedGroupId = $request->get('group_id');
             $selectedGroup = null;
-            $bVec = [];
-            $decisions = [];
+            $productivities = [];
             $labels = [];
             
             if ($selectedGroupId) {
                 $selectedGroup = WorkGroup::with('workers')->find($selectedGroupId);
                 if ($selectedGroup && $selectedGroup->workers->count() > 0) {
                     $solver = new ProductivitySolver($selectedGroup);
-                    $bVec = $solver->getBVec();
-                    $decisions = $solver->solveByGauss();
+                    $productivities = $solver->calculateProductivity();
                     
                     foreach ($selectedGroup->workers as $worker) {
                         $labels[] = $worker->last_name . ' ' . mb_substr($worker->first_name, 0, 1) . '.';
@@ -40,7 +38,7 @@ class ChartController extends Controller
                 }
             }
             
-            return view('pages.chart', compact('groups', 'selectedGroup', 'selectedGroupId', 'bVec', 'decisions', 'labels'));
+            return view('pages.chart', compact('groups', 'selectedGroup', 'selectedGroupId', 'productivities', 'labels'));
         } catch (\Exception $e) {
             Log::error('Ошибка в charts: ' . $e->getMessage());
             return view('pages.chart')->with('error', 'Ошибка при загрузке графиков');
