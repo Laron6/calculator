15fc0d917a5481a4416ba181acb700e27d701c26
Bogdan Matrosov <laroncool@gmail.com>
2026-05-10 23:14:06 +0400

    добавил отображение ошибок импорта

--- resources/views/components/alerts.blade.php
+++ resources/views/components/alerts.blade.php
@@ -9,3 +9,15 @@
         <i class="fas fa-exclamation-circle"></i> {{ session('error') }}
     </div>
 @endif
+
+@if(session('import_errors'))
+    <div class="alert alert-warning">
+        <i class="fas fa-exclamation-triangle"></i> 
+        <strong>Найдены ошибки в строках:</strong>
+        <ul>
+            @foreach(session('import_errors') as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
