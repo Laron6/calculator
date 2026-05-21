d0449132d1117d138c8c3a2d84b29045202eebea
Bogdan Matrosov <laroncool@gmail.com>
2026-05-21 20:29:35 +0400

    api.php: добавил auth:sanctum для защиты api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\CalculatorController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::get('/calculate/{groupId}', [CalculatorController::class, 'calculate']);
 });
