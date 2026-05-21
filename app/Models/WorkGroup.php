f0a65f5b32a0338650c67bd2e05020e722b1a1fb
Bogdan Matrosov <laroncool@gmail.com>
2026-05-21 20:29:33 +0400

    workgroup.php: добавил трейты и связи

--- app/Models/WorkGroup.php
+++ app/Models/WorkGroup.php
@@ -12,9 +12,14 @@ class WorkGroup extends Model
 {
     use HasFactory, WorkGroupAttributes, WorkGroupScopes, WorkGroupRelations;
     
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'user_id'];
     
     protected $casts = [
         'id' => 'integer'
     ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
